8aa5a7c754f7174820b737b8c67ab23c3a0339fa
JA-Cuartas-Micieces <48151322+JA-Cuartas-Micieces@users.noreply.github.com>
2022-01-24 16:51:39 +0100

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,12 @@
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dt=pd.read_csv("breastcancer.csv")
+dt
+
+fig=plt.scatter(dt.loc[:,"perimeter_mean"],dt.loc[:,"area_mean"])
+plt.savefig('test.pdf')
+
+plt.show()
